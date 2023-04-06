13cf130b995188599db0cd00bd776b44513f7302
Dieter Cambier <dieter.cambier@vdabcampus.be>
2023-04-06 14:55:51 +0200

    Met oefening Mandje

--- Taken/Mandje/mandje.js
+++ Taken/Mandje/mandje.js
@@ -3,9 +3,9 @@
 const aBeschikbaar = document.querySelectorAll("#beschikbaar li a");
 const spanAantal = document.getElementById("aantal");
 const ulMandje = document.getElementById("mandje");
-
 //Vul arrMandje met de items uit de localStorage.
 let arrMandje = JSON.parse(localStorage.getItem("localMandje"));
+
 /*
 Als het lokale mandje niet bestaat bij het laden van de pagina.
 Vul arrMandje met een lege array
@@ -13,9 +13,9 @@ Vul arrMandje met een lege array
 if (arrMandje == null) {
     arrMandje = [];
 }
-//Toon het aantal artikelen van arrMandje in spanAantal bij het laden van de pagina.
+//Toon het aantal artikelen in arrMandje bij het laden van de pagina.
 spanAantal.innerText = arrMandje.length;
-//Toon alle artikelen in arrMandje in ulMandje bij het laden van de pagina.
+//Toon alle artikelen in arrMandje in ulMandje
 for (const artikel of arrMandje) {
     ulMandje.appendChild(document.createElement("li")).innerText = artikel;
 }
@@ -29,7 +29,7 @@ for (const a of aBeschikbaar) {
         localStorage.setItem("localMandje", JSON.stringify(arrMandje));
         //Toon het aantal items in het mandje in spanAantal
         spanAantal.innerText = arrMandje.length;
-        //Voeg het laatst toegevoegde item in arrMandje toe als li element aan ulMandje.
-        ulMandje.appendChild(document.createElement("li")).innerText = arrMandje[arrMandje.length-1];
+        //Voeg het laatste item in arrMandje toe als li element aan ulMandje.
+        ulMandje.appendChild(document.createElement("li")).innerText = arrMandje[arrMandje.length - 1];
     }
 }
